c411b7862f41ecd51d40d082eaeed7ae09a67f96
mprem <premarem@gene>
2026-07-01 14:58:37 -0400

    add

--- tests/practice/login.spec.ts
+++ tests/practice/login.spec.ts
@@ -2,13 +2,12 @@ import {test, expect} from '@playwright/test';
 
 
 test.describe( 'login', () => {
-    test( '', async ({page}) =>{
+    test( 'Successful login', async ({page}) =>{
         await page.goto('https://practice.expandtesting.com/login');
         await expect(page).toHaveTitle('Test Login Page for Automation Testing Practice');
         await page.locator("#username").fill('practice')
-        await page.locator('#Password').fill('SuperSecretPassword!');
+        await page.locator('#password').fill('SuperSecretPassword!');
         await page.getByRole('button', { name: 'Login' }).click();
-        await page.waitForLoadState('domcontentloaded');
         await expect(page.locator('#flash')).toContainText('You logged into a secure area!');
 
     })
